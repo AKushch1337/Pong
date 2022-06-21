8ddbdfcc907f5599f059e4194fb1c9d36030a132
Artem Kushch <artemkusch2@gmail.com>
2022-06-21 13:57:40 +0300

    Added variables of where the default position of the ball is. Added comments for understanding what variables stand for. Added some properties to paddle of player 1 and 2. Added let for default scores before the game starts. Added variable of interval(will be used later).

--- index.js
+++ index.js
@@ -12,4 +12,25 @@ const player2Color = "black";
 const ballDiameter = 20;
 const ballColor = 'green';
 const ballBorderColor = 'black';
-const playerSpeed = 40;
+const playerSpeed = 40; //how fast do paddles move
+let ballDirectionX = 0; //direction where ball will be moving in X axis
+let ballDirectionY = 0; //direction where ball will be moving in Y axis
+let ballX = gameWidth / 2; //default placement of the ball
+let ballY = gameHeight / 2; //default placement of the ball
+let ballSpeed = 1;
+let player1Properties = {
+    height: 80,
+    width: 20,
+    x: 0,
+    y: gameHeight / 2
+};
+let player2Properties = {
+    height: 80,
+    width: 20,
+    x: gameWidth - 20,
+    y: (gameHeight - 80) / 2
+};
+let player1Score = 0; //score before the game start
+let player2Score = 0; //score before the game start
+let interval; //
+
